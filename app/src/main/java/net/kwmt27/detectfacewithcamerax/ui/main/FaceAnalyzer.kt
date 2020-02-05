7ea0ca969f4b0a0221efbc00f5598e57873374ca
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-02-05 23:32:28 +0900

    face解析用クラス追加

---
+++ app/src/main/java/net/kwmt27/detectfacewithcamerax/ui/main/FaceAnalyzer.kt
@@ -0,0 +1,10 @@
+package net.kwmt27.detectfacewithcamerax.ui.main
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+class FaceAnalyzer: ImageAnalysis.Analyzer {
+    override fun analyze(image: ImageProxy) {
+
+    }
+}
